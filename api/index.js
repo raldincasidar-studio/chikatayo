22181bca235e614d3f8c409f5a0c6bcaa3d10711
raldincasidar-studio <raldincasidar@gmail.com>
2025-07-15 13:38:23 +0800

    add cors vercel pro

--- api/index.js
+++ api/index.js
@@ -11,7 +11,7 @@ const cloudinary = require('cloudinary').v2;
 
 const app = express();
 app.use(cors({
-  origin: 'http://localhost:3000',
+  origin: ['http://localhost:3000', 'https://chikatayo.vercel.app'],
   credentials: true
 }));
 app.use(express.json({ limit: '100mb' }));
